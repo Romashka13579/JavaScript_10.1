daccd6473d136d03922b9267c65df2f6329b5500
Romashka <rroma13579@gmail.com>
2022-09-08 23:08:18 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var addmore = document.querySelector(".add-more");
 var btntxt = document.querySelector(".btntxt");
 var blocks = document.querySelector(".blocks");
+var block_btns = document.querySelectorAll(".block-btn");
+var blocks_s = document.querySelectorAll(".block");
+
 
 addmore.addEventListener('click', (e) => {
     if(btntxt.value != ""){
@@ -9,7 +12,21 @@ addmore.addEventListener('click', (e) => {
         var block = block_s[0].cloneNode(true);
         blocks.prepend(block);
         var block_s = document.querySelectorAll(".block");
-        var block_txt = document.querySelector(".block-txt")
-        block_txt.innerHTML = btntxt.value
+        var block_txt = document.querySelector(".block-txt");
+        var block_btns = document.querySelectorAll(".block-btn");
+        block_txt.innerHTML = btntxt.value;
+        MinusBtn(block_btns);
     }
+    btntxt.value = '';
 });
+MinusBtn(block_btns);
+function MinusBtn(block_btns){
+    block_btns.forEach(blockBtn => {
+        blockBtn.addEventListener('click', (e) => {
+            if(block_btns.length != 1){
+                e.preventDefault();
+                blockBtn.parentElement.remove();
+            }
+        });
+    });
+}
