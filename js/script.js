4ce259bb54aa68d58d99cc2a8036e80394948f6e
Romashka <rroma13579@gmail.com>
2022-09-09 00:01:51 +0200

    fifth commit

--- js/script.js
+++ js/script.js
@@ -2,19 +2,17 @@ var addmore = document.querySelector(".add-more");
 var btntxt = document.querySelector(".btntxt");
 var blocks = document.querySelector(".blocks");
 var block_btns = document.querySelectorAll(".block-btn");
-var blocks_s = document.querySelectorAll(".block");
+var block_s = document.querySelectorAll(".block");
 
 
 addmore.addEventListener('click', (e) => {
     if(btntxt.value != ""){
         e.preventDefault();
-        var block_s = document.querySelectorAll(".block");
-        var block = block_s[0].cloneNode(true);
-        blocks.prepend(block);
-        var block_s = document.querySelectorAll(".block");
+        var block = document.createElement('div');
+        block.block_txt.innerHTML = btntxt.value;
+        blocks.appendChild(block);
         var block_txt = document.querySelector(".block-txt");
-        var block_btns = document.querySelectorAll(".block-btn");
-        block_txt.innerHTML = btntxt.value;
+        var block_btns = document.querySelectorAll("block-btn");
         MinusBtn(block_btns);
     }
     btntxt.value = '';
@@ -23,9 +21,10 @@ MinusBtn(block_btns);
 function MinusBtn(block_btns){
     block_btns.forEach(blockBtn => {
         blockBtn.addEventListener('click', (e) => {
-            if(block_btns.length != 1){
+            if(block_btns.length > 2){
                 e.preventDefault();
-                blockBtn.parentElement.remove();
+                blockBtn.parentElement.style.opacity = "0"
+                setInterval( () => {blockBtn.parentElement.remove();}, 500);
             }
         });
     });
